82d79d8a03c7277de70c7dbc4706fdef39df4504
Timothy <Damiranoskilful@gmail.com>
2021-06-15 12:28:12 +0100

    commit on 'master'

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -8,8 +8,8 @@ export const homeObjOne = {
 	description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals.',
 	headline: 'Lead Generation Specialist for Online Businesses',
 	lightText: true,
-	topLine: "Marketing Agency",
-	img: '',
+	topLine: 'Marketing Agency',
+	img: require('../../images/femalered.svg'),
 	alt: 'Image',
 	start: ''
 };
